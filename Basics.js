3d142adba8b2aba408bac1f2ba10fdb40d1cff38
meghanataduru <m.taduru25@gmail.com>
2025-09-16 19:53:43 -0400

    programming 101

---
+++ Basics.js
@@ -0,0 +1,48 @@
+//Programming Basics
+
+//1. Write a function thats takes in a number and returns its sum 
+
+function sumOfNumber(num1,num2){
+    let sum = num1+num2;
+    return sum;
+}
+
+let result = sumOfNumber(5,10);
+console.log(result);
+
+
+//2. Write a function that takes in the values and returns its multiply 
+
+function multiplyOfNumber(num1,num2){
+    let multiply = num1 * num2;
+    return multiply;
+}
+let result1 = multiplyOfNumber(5,10);
+console.log(result1);
+
+//3. Write a function that takes a value and gives a square of the number 
+
+function squareOfNumber(num){
+    let square = num * num;
+    return square;
+}
+let result2 = squareOfNumber(5);
+console.log(result2);
+
+
+//4. Write a function to check eligibility to vote 
+
+function checkEligibilityToVote(age){
+    if(age < 0){
+        return "Invalid age";
+    }
+    if(age >= 18){
+        return "Eligible to vote";
+    }else{
+        return "Not eligible to vote";
+    }
+}
+
+let result3 = checkEligibilityToVote(-1);
+console.log(result3);
+
